167c17b21b6ebc5331fc78be56da70c47d977879
iamhruthiks <iamhruthiks@gmail.com>
2024-08-09 12:02:44 +0530

    implement error handling

--- controller/authContoller.js
+++ controller/authContoller.js
@@ -1,3 +1,65 @@
-exports.registerController = async () => { }
-exports.loginController = async () => { }
-exports.logoutController = async () => { }
+const userModel = require("../models/userModel")
+const errorResponse = require("../utils/errorResponse")
+
+
+// jwt token
+exports.sendToken = (user, statusCode, res) => {
+    const token = user.getSignedToken(res)
+    res.status(statusCode).json({
+        success: true,
+        token,
+    })
+}
+
+// register
+exports.registerController = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body
+
+        //existing user
+        const existingEmail = await userModel.findOne({ email })
+        if (existingEmail) {
+            return next(new errorResponse("email is already registered", 500))
+        }
+        const user = await userModel.create({ username, email, password })
+        sendToken(user, 201, res)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
+
+// login
+exports.loginController = async (req, res, next) => {
+    try {
+        const { email, passowrd } = req.body
+
+        // validation
+        if (!email || !passowrd) {
+            return next(new errorResponse("please provide email or password"))
+        }
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return next(new errorResponse("invalid creditial", 401))
+        }
+        const isMatch = await userModel.matchpassword(passowrd)
+        if (!isMatch) {
+            return next(new errorResponse("invalid creditial", 401))
+        }
+
+        // res
+        sendToken(user, 200, res)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
+
+// logout
+exports.logoutController = async (req, res) => {
+    res.clearCookie("refreshToken")
+    return res.status(200).json({
+        success: true,
+        message: "logout successfully"
+    })
+}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser")
 const colors = require("colors")
 const dotenv = require("dotenv")
 const connectDB = require("./config/db")
+const errorHandler = require("./middelware/errorMiddleware")
 
 // routes path
 const authRoutes = require("./routes/authRoutes")
@@ -23,6 +24,7 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
+app.use(errorHandler)
 
 //api routes
 app.use("/api/v1/auth", authRoutes)
