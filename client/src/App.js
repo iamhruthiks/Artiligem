e8eb87c6ee11d90ed81f8f819d3c57a4ab5b76c0
iamhruthiks <iamhruthiks@gmail.com>
2025-03-31 18:42:51 +0530

    comment out console.log statements

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom"
-import Navbar from './components/Navbar';
-import Homepage from './pages/Homepage';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import { Toaster } from 'react-hot-toast'
-import Summary from './pages/Summary';
-import Tools from './pages/Tools';
-import Paragraph from './pages/Paragraph';
-import Chatbot from './pages/Chatbot';
-import JS from './pages/JS';
-import ReactGA from "react-ga4"
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { Toaster } from "react-hot-toast";
+import Summary from "./pages/Summary";
+import Tools from "./pages/Tools";
+import Paragraph from "./pages/Paragraph";
+import Chatbot from "./pages/Chatbot";
+import JS from "./pages/JS";
+import ReactGA from "react-ga4";
 
-const ga4_id = process.env.REACT_APP_GA4
-console.log(ga4_id)
+const ga4_id = process.env.REACT_APP_GA4;
+// console.log(ga4_id)
 
-ReactGA.initialize(ga4_id)
+ReactGA.initialize(ga4_id);
 
 ReactGA.send({
   hitType: "pageview",
-  page: window.location.pathname
-})
+  page: window.location.pathname,
+});
 
 function App() {
   return (
@@ -28,14 +28,14 @@ function App() {
       <Navbar />
       <Toaster />
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/tools' element={<Tools />} />
-        <Route path='/summary' element={<Summary />} />
-        <Route path='/paragraph' element={<Paragraph />} />
-        <Route path='/chatbot' element={<Chatbot />} />
-        <Route path='/js' element={<JS />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/tools" element={<Tools />} />
+        <Route path="/summary" element={<Summary />} />
+        <Route path="/paragraph" element={<Paragraph />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/js" element={<JS />} />
       </Routes>
     </>
   );
